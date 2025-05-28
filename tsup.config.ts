56404e769ae77181c665a0d1cbb9ce0e6ede5dd0
Gioacchino Gasparella <info@gioacchinogasparella.com>
2025-05-28 10:41:06 +0200

    feat: enable source maps in tsup configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
     entry: ['src/index.ts'],
-    splitting: false,
     clean: true,
     dts: true,
+    sourcemap: true,
     format: ['esm', 'cjs'],
     target: 'node16',
     outExtension({ format }) {
