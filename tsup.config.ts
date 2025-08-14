2323fb1d6c9e571d61682767ca7ec77fc1db5a30
Gioacchino Gasparella <info@gioacchinogasparella.com>
2025-08-14 11:46:28 +0200

    feat: update test scripts and enhance test coverage for tenant-specific responses

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,27 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-    entry: ['src/index.ts'],
-    clean: true,
-    dts: true,
-    sourcemap: true,
-    format: ['esm', 'cjs'],
-    target: 'node16',
-    outExtension({ format }) {
-        return {
-            js: format === 'esm' ? '.mjs' : '.cjs',
+export default defineConfig([
+    // Build configuration for plugin
+    {
+        entry: ['src/index.ts'],
+        clean: true,
+        dts: true,
+        sourcemap: true,
+        format: ['esm', 'cjs'],
+        target: 'node16',
+        outExtension({ format }) {
+            return {
+                js: format === 'esm' ? '.mjs' : '.cjs',
+            }
         }
+    },
+    // Build configuration for tests
+    {
+        entry: ['test/**/*.test.ts'],
+        sourcemap: true,
+        clean: false, // false to keep plugin build files   
+        format: ['esm'],
+        target: 'node16',
+        outDir: 'dist/test',
     }
-})
+])
